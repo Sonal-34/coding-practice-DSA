62ccd0352ffbb9fe88b034c387d2b50541918b4b
Sonal Parashar <sonalparashar4@gmail.com>
2026-01-02 14:56:55 +0530

    Create TwoSum.java

---
+++ src/Array/TwoSum.java
@@ -0,0 +1,37 @@
+package Array;
+
+import java.util.HashMap;
+
+public class TwoSum {
+    public static void main(String[] args) {
+        int arr[]={2,7,11,15};
+        int target = 9;
+        int[] result = twoSum(arr, target);
+
+        System.out.println("Index 1: " + result[0]);
+        System.out.println("Index 2: " + result[1]);
+
+    }
+    public static int[] twoSum(int[] nums, int target) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++)
+        {
+
+            int complement = target - nums[i];
+
+
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+
+         
+            map.put(nums[i], i);
+        }
+
+        return new int[0];
+            }
+        }
+
+
