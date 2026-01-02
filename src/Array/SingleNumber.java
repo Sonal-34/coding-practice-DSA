c92f9d7aa7977d76bc2f4aaee2a97247b284959f
Sonal Parashar <sonalparashar4@gmail.com>
2026-01-02 21:20:00 +0530

    Create SingleNumber.java

---
+++ src/Array/SingleNumber.java
@@ -0,0 +1,19 @@
+package Array;
+
+public class SingleNumber {
+    public static void main(String[] args) {
+        int arr[]={4,1,2,1,2,3,4};
+        int result=singleNumber(arr);
+        System.out.println("Unique number from the array is: " + result);
+    }
+    public static int singleNumber(int[] arr)
+    {
+        int xorResult = 0;
+        for(int i=0;i<arr.length;i++)
+        {
+            xorResult ^= arr[i];
+        }
+        return xorResult;
+
+    }
+}
