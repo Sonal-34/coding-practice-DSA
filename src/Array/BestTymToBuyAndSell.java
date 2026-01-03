b00cb65ba92aa2811d2b3d5e09e2ffb7cb3ad19c
Sonal Parashar <sonalparashar4@gmail.com>
2026-01-03 13:36:21 +0530

    Update BestTymToBuyAndSell.java

--- src/Array/BestTymToBuyAndSell.java
+++ src/Array/BestTymToBuyAndSell.java
@@ -1,11 +1,13 @@
 package Array;
 
-public class BestTymToBuyAndSell {
+public class BestTymToBuyAndSell
+{
     public static void main(String[] args) {
         int arr[]={7,1,5,3,6,4};
         System.out.println("Maximum profit is: " + maxProfit(arr));
     }
-    public static int maxProfit(int[] prices) {
+    public static int maxProfit(int[] prices)
+    {
         int bestbuy = prices[0];
         int maxProfit = 0;
 
