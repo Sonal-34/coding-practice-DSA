6980a54a4d6e99c591169edb1fe72ee30caf191a
Sonal Parashar <sonalparashar4@gmail.com>
2026-01-09 23:07:01 +0530

    Create ProductOfArayExceptSelf.java

---
+++ src/Array/ProductOfArayExceptSelf.java
@@ -0,0 +1,33 @@
+package Array;
+import java.util.Arrays;
+public class ProductOfArayExceptSelf {
+    public static void main(String[] args)
+    {
+        int[] nums = {1, 2, 3, 4};
+
+        ProductOfArayExceptSelf obj = new ProductOfArayExceptSelf();
+        int[] result = obj.productExceptSelf(nums);
+
+        System.out.println("Product of array except self:");
+        System.out.println(Arrays.toString(result));
+    }
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int ans[] = new int[n];
+        Arrays.fill(ans,1);
+
+        //calculate prefix and stored in ans
+        for(int i=1;i<n;i++)
+        {
+            ans[i] = ans[i-1] * nums[i-1];
+        }
+
+        //calculate suffix and update ans
+        int suffix = nums[n-1];
+        for (int i = n - 2; i >= 0; i--) {
+            ans[i] = ans[i] * suffix; // prefix * suffix
+            suffix *= nums[i];
+        }
+        return ans;
+    }
+}
